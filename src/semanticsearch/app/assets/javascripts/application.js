9265ee758e656df93ba0aac033c63c7e2d4a5b69
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-05-05 22:33:22 +0200

    Upgraded search interface

--- src/semanticsearch/app/assets/javascripts/application.js
+++ src/semanticsearch/app/assets/javascripts/application.js
@@ -6,6 +6,63 @@
 
 $(document).ready(function() {
 
-  $('.search-icon').hide();
+  // Display value is key, Dublin Core property is value
+  var filters = {
+    'Theme': 'theme',
+    'Title': 'title',
+    'Author': 'author',
+    'Subject': 'subject'
+  };
+
+  var operator = 'WTF';
+
+  $('.search-icon > a').click(function(ev) {
+      ev.preventDefault();
+      $('html, body').animate({ scrollTop: $(document).height() }, 600);
+  });
+
+  /* Overlay handling */
+  $('#overlay-and-button').click(function(ev) {
+    ev.preventDefault();
+    operator = 'AND';
+    $('.overlay').fadeOut(200);
+    addFilter();
+    initDropdown();
+  });
+  $('#overlay-or-button').click(function(ev) {
+    ev.preventDefault();
+    operator = 'OR';
+    $('.overlay').fadeOut(200);
+    addFilter();
+    initDropdown();
+  });
+
+  var addFilterHandler = function(ev) {
+    ev.preventDefault();
+    $('.overlay').fadeIn(200);
+  }
+
+  var initDropdown = function() {
+    Object.keys(filters).forEach(function(key) {
+      $('.search-dropdown').append('<option value=' + filters[key] + '>' + key + '</option>');
+    });
+  };
+  var template = $('#filter-template').html();
+  var addButtonTemplate = $('#add-button-template').html();
+  var addFilter = function() {
+    $('#search-filter-container').append(template);
+    // Show 'AND' or 'OR'
+    $('.add-button-container').html(operator);
+    // Add 'add' button to the last filter
+    $('.add-button-container').last().html(addButtonTemplate).children('a').click(addFilterHandler);
+  }
+
+  /**
+   * Initialization
+   *
+   * */
+  addFilter();
+  initDropdown();
+  $('.overlay').hide();
 
 });
